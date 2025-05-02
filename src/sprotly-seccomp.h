d53eb02ced885014e77899e956144a1377514e4e
Andrew Clayton <ac@sigsegv.uk>
2025-05-02 21:07:12 +0100

    Fix an include header guard
    GCC 15 handily warned about the following
    
      In file included from sprotly.c:49:
      sprotly-seccomp.h:17: warning: header guard ‘_SPROTLY_SECCOMP_H_’ followed by ‘#define’ of a different macro [-Wheader-guard]
         17 | #ifndef _SPROTLY_SECCOMP_H_
      sprotly-seccomp.h:18: note: ‘_SPROtLY_SECCOMP_H_’ is defined here; did you mean ‘_SPROTLY_SECCOMP_H_’?
         18 | #define _SPROtLY_SECCOMP_H_
    
    Indeed we had a 't' instead of 'T' in the name.
    
    Signed-off-by: Andrew Clayton <ac@sigsegv.uk>

--- src/sprotly-seccomp.h
+++ src/sprotly-seccomp.h
@@ -4,6 +4,8 @@
  * Copyright (c) 2017		Securolytics, Inc.
  *				Andrew Clayton <andrew.clayton@securolytics.io>
  *
+ * Copyright (c) 2025		Andrew Clayton <ac@sigsegv.uk>
+ *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
  * version 2, as published by the Free Software Foundation.
@@ -15,7 +17,7 @@
  */
 
 #ifndef _SPROTLY_SECCOMP_H_
-#define _SPROtLY_SECCOMP_H_
+#define _SPROTLY_SECCOMP_H_
 
 void init_seccomp(void);
 
